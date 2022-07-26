28ab889b350bfd32520b26e810b6efd0acfeae61
Vladislav Gushtyn <vladislav3372@gmail.com>
2022-07-27 00:46:10 +0300

    Add admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from posts.models import Post, Tag
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("author", "title", "slug", "created_at")
+    fields = ("author", "title", "slug", "text", "created_at")
+    readonly_fields = ("created_at",)
+    search_fields = ("title", "slug", "text")
+    raw_id_fields = ("author",)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    search_fields = ("title",)
