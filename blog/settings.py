56638865ba4ca30980472261599cfe6685742bcc
Vladislav Gushtyn <vladislav3372@gmail.com>
2022-07-29 14:10:25 +0300

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'posts',
     'profiles',
+    'shop',
 ]
 
 MIDDLEWARE = [
@@ -77,10 +78,14 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.environ.get('DATABASE_PATH', BASE_DIR / 'db.sqlite3'),
-    }
+   "default": {
+       "ENGINE": "django.db.backends.postgresql",
+       "NAME": "django",
+       "USER": "django",
+       "PASSWORD": "django",
+       "HOST": "localhost",
+       "PORT": 5432,
+   }
 }
 
 
