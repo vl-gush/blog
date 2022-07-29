1fc5fb00da0da4da9f5e3acf52e55dc0b4422aea
Vladislav Gushtyn <vladislav3372@gmail.com>
2022-07-29 14:10:05 +0300

    Add Product and Purchase models

---
+++ shop/models.py
@@ -0,0 +1,28 @@
+from django.conf import settings
+from django.db import models
+
+COLOR = (('red', 'Red color'), ('green', 'Green color'), ('white', 'White color'))
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    color = models.CharField(
+        max_length=200,
+        choices=COLOR,
+        blank=True,
+        null=True,
+    )
+    cost = models.IntegerField()
+
+    def __str__(self):
+        return f"Product: {self.title}"
+
+
+class Purchase(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, related_name="purchases", on_delete=models.CASCADE
+    )
+    product = models.ForeignKey(
+        Product, related_name="purchases", on_delete=models.CASCADE
+    )
+    count = models.IntegerField()
