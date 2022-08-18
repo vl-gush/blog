4c92215f579f9827eeeb36a7ec6f0ba9bfb8b44a
Vladislav Gushtyn <vladislav3372@gmail.com>
2022-08-18 23:44:02 +0300

    Add test_api.py

---
+++ tests/test_api.py
@@ -0,0 +1,29 @@
+import pytest
+
+from django.test.client import Client
+
+from posts.models import Post
+from tests.factories import PostFactory
+
+
+@pytest.mark.django_db
+class TestViews:
+
+    def setup_method(self):
+        self.client = Client()
+
+    def test_post_list(self):
+        PostFactory.create_batch(5)
+        response = self.client.get("/api/posts/")
+
+        assert response.status_code == 200
+        assert len(response.data) == 5
+
+    def test_post_create(self):
+        data = {"title": "title", "slug": "slug", "text": "text"}
+        response = self.client.post("/api/posts/", data=data)
+        assert response.status_code == 201
+
+        response = self.client.get("/api/posts/")
+        assert response.status_code == 200
+        assert len(response.data) == 1
