bd9c2b08baf5562db5cdbcca8f3e52067374b1fc
Vladislav Gushtyn <vladislav3372@gmail.com>
2022-09-21 01:09:28 +0300

    Update forms and views in posts

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,26 @@
-from django.shortcuts import render
 
+
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
+from posts.forms import PostForm
 from posts.models import Post
 
 
 def index(request):
     posts = Post.objects.all()
     return render(request, "index.html", {"posts": posts})
+
+
+def post_add(request):
+    if not request.user.is_authenticated:
+        return HttpResponse("You aren't authenticated!")
+
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            Post.objects.create(author=request.user, **form.cleaned_data)
+            return redirect("index")
+    else:
+        form = PostForm()
+    return render(request, "post_add.html", {"form": form})
