c3fa3fef3b9daccdc11d0042145d293cbc1235cc
Vladislav Gushtyn <vladislav3372@gmail.com>
2022-09-21 01:10:47 +0300

    Update views in shop

--- shop/views.py
+++ shop/views.py
@@ -1,16 +1,21 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from django.core.paginator import Paginator
 
 from shop.models import Product
 from shop.services import get_sorted_product
 
 
 def products(request):
-    if request.GET.get('color'):
-        product_list = Product.objects.filter(color=request.GET.get('color'))
+    if request.GET.get("color"):
+        product_list = Product.objects.filter(color=request.GET.get("color"))
     else:
         product_list = Product.objects.all()
-    order_by = request.GET.get('order_by')
 
+    order_by = request.GET.get("order_by")
     product_list = get_sorted_product(product_list, order_by)
-    return render(request, "index.html", {"product_list": product_list})
+
+    page_number = request.GET.get("page")
+    paginator = Paginator(product_list, 15)
+    paginator = paginator.get_page(page_number)
+
+    return render(request, "index.html", {"paginator": paginator})
